6ca8bb1b3b5de6e70478d3b464057189ca28d462
Katherine ChengLi <katchengli@gmail.com>
2015-03-07 13:32:56 -0500

    changed sttings

--- DIYCalc/DIYCalc/settings.py
+++ DIYCalc/DIYCalc/settings.py
@@ -57,11 +57,11 @@ WSGI_APPLICATION = 'DIYCalc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.sqlite3',
-#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+# DATABASES = {
+    # 'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+       # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+   # }
 # }
 
 # Parse database configuration from $DATABASE_URL
