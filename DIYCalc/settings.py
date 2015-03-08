88845aaf3cf0d84b749ee86647e0e6fd416abec5
Katherine ChengLi <katchengli@gmail.com>
2015-03-07 19:55:17 -0500

    trying to fix Heroku setup

--- DIYCalc/settings.py
+++ DIYCalc/settings.py
@@ -84,7 +84,7 @@ ALLOWED_HOSTS = ['*']
 # Static asset configuration
 import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
+STATIC_ROOT = '/app/staticfiles'
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
