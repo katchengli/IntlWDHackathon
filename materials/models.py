b76f559b0a6129a4f4d1f72d69b2124e99d0cac8
Natalia Castillo <nich13@gmail.com>
2015-03-08 18:17:46 -0400

    Fixed urls.py

--- materials/models.py
+++ materials/models.py
@@ -4,7 +4,7 @@ from django.forms import Form
 class Material(models.Model):
     name=models.CharField(max_length=200)
     date_entered=models.DateTimeField('Date Entered', auto_now_add=True)
-    #unit=models.ForeignKey('management.Unit')
+    unit=models.ForeignKey('management.Unit')
     #category = models.ForeignKey('Category')
     price=models.DecimalField(max_digits=6, decimal_places=2)
     quantity=models.DecimalField(max_digits=6, decimal_places=2)
